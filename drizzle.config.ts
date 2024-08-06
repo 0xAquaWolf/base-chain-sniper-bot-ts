62fef70ef6e04158b529c02f5321e5077854ddb4
0xAquaWolf <0xaquawolf@protonmail.com>
2024-08-06 14:29:43 -0400

    updated for postgres

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './src/database/schema.ts',
+  out: './drizzle',
+  dbCredentials: {
+    url: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  },
+});
