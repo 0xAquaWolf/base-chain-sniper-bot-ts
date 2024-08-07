62f0525c8925cb694c7d843e7f3ee47ffee642e0
0xAquaWolf <0xaquawolf@protonmail.com>
2024-08-07 10:46:16 -0400

    archieved previous implimentation

--- src/database/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema.ts';
-
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
-// for query purposes
-const queryClient = postgres(connectionString);
-export const db = drizzle(queryClient, { schema });
